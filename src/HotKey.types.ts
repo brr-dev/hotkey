a67ede84ddb20be8b701028e45073051fffd7f8c
bramirez96 <bran.ramirez.don@gmail.com>
2024-09-20 23:07:14 -0700

    HotKey option types

---
+++ src/HotKey.types.ts
@@ -0,0 +1,20 @@
+/*
+ * @author Brandon Ramirez <brandon@brr.dev>
+ * @copyright Copyright (c) 2024
+ */
+
+export type HotKeyCallback = (event: KeyboardEvent) => false | void;
+
+export type HotKeyOptions = {
+    // Key and callback are required
+    key: string;
+    callback: HotKeyCallback;
+
+    // Optional target override, defaults to the document body
+    target: HTMLElement;
+
+    // Optional modifiers, default to false
+    ctrl?: boolean;
+    alt?: boolean;
+    shift?: boolean;
+};
